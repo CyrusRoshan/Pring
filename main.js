cdbc474afa2a3779930f6c0f3cb1a932b7b05283
Cyrus Roshan <CyrusRoshan@users.noreply.github.com>
2016-02-15 07:50:24 -0600

    update, give some functionality, calculate basic info

--- main.js
+++ main.js
@@ -16,8 +16,6 @@ var mb = menubar({
   height: 300,
 });
 
-//mb.tray.setImage(iconRed);
-
 var settings = {
   server: '8.8.8.8',
 }
@@ -25,11 +23,44 @@ var settings = {
 
 mb.on('ready', function ready () {
   var ping = new Ping(settings.server);
-  setInterval(() => pingServer(ping), 500);
-});
+  var pingTimes = [];
+  const pingsStored = 30;
 
-function pingServer(obj) {
-  obj.send(function(err, ms) {
-    console.log('#'.repeat(ms) + '   ' + ms);
-  });
-}
+  function update(array, value) {
+    if (array.length + 1 > pingsStored) {
+      array.shift();
+    }
+    array.push(value);
+
+    var total = 0;
+    var count = 0;
+    var dropped = 0;
+    for (var i = 0; i < array.length; i++) {
+      if (array[i]) {
+        total += array[i];
+        count++;
+      } else {
+        dropped++;
+      }
+    }
+
+    var average = total/count;
+    var packetLoss = dropped/array.length;
+
+    //change icon to red only if current icon is black and vice versa
+    if (pingTimes[pingTimes.length - 2] && pingTimes[pingTimes.length - 1]) {
+      mb.tray.setImage(iconBlack);
+    } else if (!pingTimes[pingTimes.length - 2] && !pingTimes[pingTimes.length - 1]) {
+      mb.tray.setImage(iconRed);
+    }
+  }
+
+
+  setInterval(() => {
+    ping.send(function (err, ms) {
+      update(pingTimes, ms);
+    });
+  }, 1000);
+
+
+});
